e56de8a4b6d6b17d9c3fd5bdcce595bbf06dbc6b
Uduak Umanah <96398168+Eyakenojnr@users.noreply.github.com>
2022-10-06 04:46:47 +0100

    concat 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,6 @@
-iii
+#!/usr/bin/node
+const fs = require('fs');
 
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
