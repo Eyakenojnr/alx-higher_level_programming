43c575003eeff0abdfe37b457dbe23d9ea88249d
Uduak Umanah <eyakenojnr@gmail.com>
2023-05-02 05:34:43 +0100

    Print ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Print the English alphabet in lowercase, not followed by a new line."""
+"""Print English alphabet in lowercase, not followed by a new line."""
 
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for letter in range(ord('a'), ord('z')+1):
+    print(chr(letter), end='')
