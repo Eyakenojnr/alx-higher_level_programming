7240939ccb2afd1b3bda8343ac33d5d97b9dd94c
Uduak Umanah <eyakenojnr@gmail.com>
2023-05-12 21:23:52 +0100

    Build own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+    """Import all functions from <calculator_1.py> and handle basic
+    operations."""
     import sys
+    from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    # If number of arguments is not 3, print usage to stderr and exit
+    args = len(sys.argv) - 1
+    if args != 3:
+        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
+    operator = sys.argv[2]
+    legible_operator = ['+', '-', '*', '/']
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
+    # If invalid operator is passed, print error message to stderr and exit
+    if operator not in legible_operator:
+        sys.stderr.write("Unknown operator. Available operators: +, -, * and /\n")
+        sys.exit(1)
+    else:
+        if operator == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif operator == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        else:
+            print("{} / {} = {}".format(a, b, div(a, b)))
