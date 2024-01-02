b2ac6eaabe3ad84c96b22cfe969c0e59b78a70ce
Uduak Umanah <eyakenojnr@gmail.com>
2024-01-02 21:33:23 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
-
+"""lists all states from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    import MySQLdb
+    import sys
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name, port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE LEFT(name, 1) = 'N' ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error connecting to the database: ", e)
+        sys.exit(1)
