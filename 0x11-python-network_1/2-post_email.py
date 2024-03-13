70ec1ef76d8d354d5aae71da999faeb93637fc70
Uduak Umanah <eyakenojnr@gmail.com>
2024-03-13 01:34:43 +0100

    `POST` an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,10 +11,17 @@ import urllib.request
 
 
 if __name__ == "__main__":
+    # Get URL and email from command-line arguments
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    email = sys.argv[2]
+
+    # Parse and encode data to be sent in the POST request
+    value = {"email": email}
     data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
+    # Creating Request object
+    req = urllib.request.Request(url, data)
+
+    # Sending the request and getting the response
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
