b1e1ee95853ccd84acccce8aeb0a1d3e7bee9ac5
Uduak Umanah <eyakenojnr@gmail.com>
2024-03-21 01:26:49 +0100

    List last 10 commits of given GitHub repository

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
+"""lists the 10 most recent commits on a given GitHub repository "rails" by the user "rails"
+All commits are printed by: `<sha>: <author name>` (one by line)
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    repo = sys.argv[1]
+    owner = sys.argv[2]
 
-    r = requests.get(url)
+    r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
     commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+            sha = commits[i].get("sha")
+            author_name = commits[i].get("commit").get("author").get("name")
+            print(f"{sha}: {author_name}")
     except IndexError:
         pass
+
+"""
+// Alternative to the try block //
+
+for commit in commits:
+    sha = commit['sha']
+    author_name = commit['commit']['author']['name']
+    print(f"{sha}: {author_name}")
+"""
