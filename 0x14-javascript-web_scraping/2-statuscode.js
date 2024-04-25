4a5e883836eea46293e52f7e3bcb52ec21ed09d4
Uduak Umanah <eyakenojnr@gmail.com>
2024-04-25 22:50:41 +0100

    Display status code of a `GET` request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,6 @@
 const request = require('request');
 const URL = process.argv[2];
 
-request(URL, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+request(URL, (err, response) => {
+  console.log('code: ', response.statusCode);
 });
