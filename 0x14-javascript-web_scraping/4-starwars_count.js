3aa662cf3d93c0ca2c9b16a9f696866fe6c4cfbe
Uduak Umanah <eyakenojnr@gmail.com>
2024-05-10 20:53:41 +0100

    Print number of movies the character "Wedge Antilles" is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,25 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = 18
 
 request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else if (response.statusCode === 200) {
-    const films = JSON.parse(body);
+    const films = JSON.parse(body).results;
     let count = 0;
 
-    films['results'].forEach(film => {
-      if (film['characters'].includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
-        count++;
+    // Iterate through films
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      // Iterate through characters of each film
+      for (const charsIndex in filmChars) {
+        // If selected film includes character ID 18, increment counter
+        if (filmChars[charsIndex].includes('18')) {
+          count++;
+        }
       }
-    });
+    }
 
     console.log(count);
   } else {
