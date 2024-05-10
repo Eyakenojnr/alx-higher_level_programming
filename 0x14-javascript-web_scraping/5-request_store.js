7f0a20f0367b4d71aa329ddf4fc82bbe7997d94b
Uduak Umanah <eyakenojnr@gmail.com>
2024-05-10 22:06:29 +0100

    Get contents of a webpageand store in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-request(process.argv[2], function (err, response, body) {
+request(url, (err, response, body) => {
   if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
+    fs.writeFileSync(filePath, body);
   }
 });
