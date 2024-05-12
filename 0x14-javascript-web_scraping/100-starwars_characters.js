ea108dbbe41f23fd0f017822ac197120dbbcea28
Uduak Umanah <eyakenojnr@gmail.com>
2024-05-12 05:41:55 +0100

    Print all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,29 @@
 #!/usr/bin/node
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+// Two requests are made; First, to get the movie characters which
+// are all urls pointing to another JSON data in which the character
+// is contained in 'name'. The second GET request requests for this
+// data
+request(url, (err, response, body) => {
+  if (!err && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (err, response, body) => {
+        if (!err && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        } else {
+          console.error('An error occurred while fetching character data.');
+        }
+      });
     });
+  } else {
+    console.error('An error occured while fetching movie data.');
   }
 });
