4194fa32f0522c6fa785769656d20024591c22d1
Uduak Umanah <eyakenojnr@gmail.com>
2024-06-15 12:24:28 +0100

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// Updates text color of the <header> element to red ('#FF0000') when
+// the user clicks on the tag DIV#red_header
+
+$(document).ready(() => {
+	$('DIV#red_header').click(() => {
+		$('header').css('color', '#FF0000');
+	});
+});
